0750dd150eb765803311a9974839855a017e9253
02ananyaa <151650735+02ananyaa@users.noreply.github.com>
2025-01-26 18:03:23 +0530

    Initial commit

---
+++ src/main/java/com/security/controller/SecurityController.java
@@ -0,0 +1,16 @@
+//package com.security.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//
+//@RestController
+//public class SecurityController {
+//
+//	@GetMapping("/")
+//	public String HomePage(HttpServletRequest request) {
+//		return "Welcome ANANYA  "+request.getRequestedSessionId();
+//	}
+//
+//}
